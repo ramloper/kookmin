588a7631a6e3bf4d0f35590d4c917fe6f4f8fd38
wooram <rla7360@gmail.com>
2023-05-24 17:40:34 +0900

    도서 업로드 구현 완료

--- src/main/java/org/kookmin/demo/controller/AdminController.java
+++ src/main/java/org/kookmin/demo/controller/AdminController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.kookmin.demo.domain.Rental;
 import org.kookmin.demo.dto.request.education.EducationSaveDTO;
 import org.kookmin.demo.service.EducationService;
-import org.kookmin.demo.service.MemberService;
 import org.kookmin.demo.service.RentalService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -13,13 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.List;
 
 @Controller
@@ -30,9 +26,32 @@ public class AdminController {
     private final EducationService educationService;
     private final RentalService rentalService;
 
-    String desktopPath = System.getProperty("user.home") + "/바탕 화면/";
-    String savePath = desktopPath + "wooram/Rental/demo/src/main/resources/static/image/";
+    @Value("${file.path}")
+    String savePath;
 
+    @PostMapping("/education/save")
+    public String saveEducation(EducationSaveDTO dto) {
+        // 도서 정보 및 업로드된 파일 처리
+        MultipartFile file = dto.getFile();
+        if (file != null && !file.isEmpty()) {
+            try {
+                // 파일 저장 경로
+                // 파일 이름
+                String fileName = file.getOriginalFilename();
+                // 파일 저장
+                file.transferTo(new File(savePath + fileName));
+                // 파일 이름을 DTO에 설정
+                dto.setFile(file);
+            } catch (IOException e) {
+                // 파일 저장 실패 시 예외 처리
+                e.printStackTrace();
+            }
+        }
+
+        // 도서 정보 저장 로직
+        educationService.saveEducation(dto);
+        return "redirect:/admin/page";
+    }
     @PostMapping("/ok")
     public String rentalOk(Integer id, Model model){
         rentalService.okRental(id);
@@ -49,30 +68,7 @@ public class AdminController {
 
         return "admin";
     }
-    @PostMapping("/education/save")
-    public String saveEducation(EducationSaveDTO dto) {
-        // 도서 정보 및 업로드된 파일 처리
-        MultipartFile file = dto.getFile();
-        if (file != null && !file.isEmpty()) {
-            try {
-                // 파일 저장 경로
-                // 파일 이름
-                String fileName = file.getOriginalFilename();
-                savePath = URLDecoder.decode(savePath, "UTF-8");
-                // 파일 저장
-                file.transferTo(new File(savePath + fileName));
-                // 파일 이름을 DTO에 설정
-                dto.setFile(file);
-            } catch (IOException e) {
-                // 파일 저장 실패 시 예외 처리
-                e.printStackTrace();
-            }
-        }
 
-        // 도서 정보 저장 로직
-        educationService.saveEducation(dto);
-        return "redirect:/admin/page";
-    }
 
 }
 
